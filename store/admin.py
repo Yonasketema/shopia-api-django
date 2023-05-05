21b97f26a1ddef774b105595ff59459b71212454
yonasketema <yonask.kitaw@gmail.com>
2023-05-05 10:01:12 +0300

    create product admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description',
+                    'price', 'last_update', 'image_uri']
--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2,)
+    last_update = models.DateTimeField(auto_now=True)
+    image_uri = models.CharField(max_length=255)

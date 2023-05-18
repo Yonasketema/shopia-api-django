4143d63495d903bf3c5967d51b83ec44747632cf
yonasketema <yonask.kitaw@gmail.com>
2023-05-18 12:37:04 +0300

    display image thumbnail

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html, urlencode
 from . import models
 
 
@@ -7,10 +8,26 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'id']
 
 
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage
+    readonly_fields = ['thumbnail']
+
+    def thumbnail(self, instance):
+        if instance.image.name != '':
+            return format_html(f'<img src="{instance.image.url}" class="thumbnail" />')
+        return ''
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title',
                     'price', 'last_update']
+    inlines = [ProductImageInline]
+
+    class Media:
+        css = {
+            'all': ['style.css']
+        }
 
 
 @admin.register(models.ProductImage)

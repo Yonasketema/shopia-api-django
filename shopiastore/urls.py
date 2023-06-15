3edbc4ce2af4498811e78e76e521ef5e53fe97e9
yonasketema <yonask.kitaw@gmail.com>
2023-06-15 20:02:11 +0300

    add homepage

--- shopiastore/urls.py
+++ shopiastore/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('store/', include('store.urls'))
 

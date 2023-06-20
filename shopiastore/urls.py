d189128eeb63850e934707309f2b40815e4060bd
yonasketema <yonask.kitaw@gmail.com>
2023-06-20 14:16:19 +0300

    fix:media url

--- shopiastore/urls.py
+++ shopiastore/urls.py
@@ -24,8 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls'))
 
-]
-
-
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

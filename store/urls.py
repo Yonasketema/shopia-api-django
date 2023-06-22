00cf927baade83b81f38576eb7b13d19137fa831
yonasketema <yonask.kitaw@gmail.com>
2023-06-22 20:03:04 +0300

    fix :retrieve review

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-from rest_framework_nested import routers
+
 
 from . import views
-
-routers = routers.DefaultRouter()
 
 urlpatterns = [
     path('product/<int:pk>', views.ProductDetailView.as_view(),
          name='product-detail'),
     path('product/', views.productList),
     path('category/', views.categoryList),
-    path('review/<int:pk>', views.ProductReview.as_view())
+    path('review/<int:pk>', views.ProductReviewView.as_view())
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 
 
 from .models import Product, Category, Review
@@ -33,7 +35,7 @@ def productList(request):
     return Response(serializer.data)
 
 
-class ProductReview(ListCreateAPIView):
+class ProductReviewView(CreateAPIView,  RetrieveAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 

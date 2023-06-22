45fa2cbefc2c1d881ebc3ead7e5db9f61c909ce6
yonasketema <yonask.kitaw@gmail.com>
2023-06-22 16:34:59 +0300

    fix : allow origins header

--- shopiastore/settings/base.py
+++ shopiastore/settings/base.py
@@ -116,7 +116,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
 }
+
+
 CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://shopia-et.vercel.app/",

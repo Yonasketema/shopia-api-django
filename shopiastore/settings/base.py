8715b66306049900fc04dba7efa545cb40f7c2de
yonasketema <yonask.kitaw@gmail.com>
2023-06-22 16:56:13 +0300

    fix : allow origins header

--- shopiastore/settings/base.py
+++ shopiastore/settings/base.py
@@ -121,23 +121,5 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOW_METHODS = [
-    "GET",
-]
 
-CORS_ALLOW_HEADERS = (
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-    "cache-control",
-    "pragma",
-
-)
-
-
+ 

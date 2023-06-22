8c319b336784040abb967e4ca7f40cff3cc906fb
yonasketema <yonask.kitaw@gmail.com>
2023-06-22 16:08:12 +0300

    fix : allow origins header

--- shopiastore/settings/base.py
+++ shopiastore/settings/base.py
@@ -117,20 +117,17 @@ REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://shopia-et.vercel.app",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
-    "http://localhost:5173",
-]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+  'accept',
+  'accept-encoding',
+  'authorization',
+  'content-type',
+  'origin',
+  'dnt',
+  'user-agent',
+  'x-csrftoken',
+  'x-requested-with']
+CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 
-CORS_ALLOW_METHODS = [
-    "GET",
-]
-
-
-CORS_ORIGIN_ALLOW_ALL = False
-
-CORS_ORIGIN_WHITELIST = (
-   'https://shopia-et.vercel.app',
-)
